313266dab1cf373321649f101dd6f2bd5d952229
dnikki <dnikita3101@gmail.com>
2021-05-15 21:09:05 +0530

    Added functions to cardviews

--- app/src/main/java/com/example/healthreminder/MainActivity.java
+++ app/src/main/java/com/example/healthreminder/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.healthreminder;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void medicine(View view) {
+        Toast.makeText(this, "Medicine layout is in progess", Toast.LENGTH_SHORT).show();
+    }
+
+    public void water(View view) {
+        Toast.makeText(this, "Water layout is in progess", Toast.LENGTH_SHORT).show();
+    }
+
+    public void food(View view) {
+        Toast.makeText(this, "Food layout is in progess", Toast.LENGTH_SHORT).show();
+    }
+
+    public void exercise(View view) {
+        Toast.makeText(this, "Exercise layout is in progess", Toast.LENGTH_SHORT).show();
     }
 }
