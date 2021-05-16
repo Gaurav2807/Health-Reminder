4eea87b6e8f889c7ecf98c59dfe92ddbe48b95f7
Gaurav2807 <72298670+Gaurav2807@users.noreply.github.com>
2021-05-16 10:35:13 +0530

    fix #7 (#9)
    Co-authored-by: Gaurav Gupta <30961298+28Gaurav@users.noreply.github.com>

---
+++ app/src/main/java/com/example/healthreminder/splash_screen.java
@@ -0,0 +1,26 @@
+package com.example.healthreminder;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class splash_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(splash_screen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 2000);
+    }
+    }
